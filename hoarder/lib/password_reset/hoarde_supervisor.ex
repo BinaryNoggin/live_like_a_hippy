9c81d0c2c09149067ed4f85388859d78bc7d0d36
Amos L King <amos.l.king@gmail.com>
2017-02-20 13:18:37 -0600

    Updates talk slides
    Amos King @adkron <amos@binarynoggin.com>

--- hoarder/lib/password_reset/hoarde_supervisor.ex
+++ hoarder/lib/password_reset/hoarde_supervisor.ex
@@ -2,7 +2,7 @@ defmodule PasswordResetHoard.Supervisor do
   use Supervisor
 
   def start_link do
-    Supervisor.start_link(__MODULE__, :ok)
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
